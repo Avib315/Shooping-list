5006c1c6710eff032937e2fea6c4be8b92bd8f5d
Avib315 <106652038+Avib315@users.noreply.github.com>
2024-01-16 15:18:44 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -6,24 +6,28 @@ const CategoryModel = require("./Models/Categories");
 const OrderModel = require("./Models/Orders"); 
 const fs = require("fs")
 const uri = "mongodb+srv://coralsh286:orcal12865893@clusterdev.xzorssw.mongodb.net/test?retryWrites=true&w=majority";
-
+const arrCategories = require('./Categories.json')
+let isConnectedToMongoDb = false;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 mongoose.connect(uri);
-
 mongoose.connection.on('connected', () => {
+    isConnectedToMongoDb = true
     console.log('Connected to MongoDB');
 });
 
 app.get('/getCategoryList', async (req, res) => {
     try {
-        const categories = await CategoryModel.find();
+        let categories = arrCategories; // במידה ואין הרשאה לכתובת מחשב לקבל נתונים ממונגו האפליקציה תמשיך לרוץ ויזואלית
+        if(isConnectedToMongoDb){
+             categories = await CategoryModel.find();
+        }
         res.status(200).send(categories);
     } catch (error) {
+        res.status(500).send(arrCategories);
         console.error('Error in /getCategoryList:', error);
-        res.status(500).send(error);
     }
 });
 const addToOrderInfo = (obj)=> {
@@ -42,7 +46,7 @@ app.post('/addOrder', async (req, res) => {
     try {
 
         const { fullName, address, email, arrOrder } = req.body;
-
+        if(isConnectedToMongoDb){
         const newOrder = new OrderModel({
             fullName,
             address,
@@ -51,11 +55,13 @@ app.post('/addOrder', async (req, res) => {
         });
 
         const savedOrder = await newOrder.save();
-        addToOrderInfo({
-            userID:savedOrder._id,
-            arrOrder:arrOrder,
-        })
         res.status(201).send({order: savedOrder,});
+    }
+    addToOrderInfo({
+        userID:savedOrder ? savedOrder._id : "",
+        arrOrder:arrOrder,
+    })
+    res.status(201).send(true)
     } catch (error) {
         console.error( error);
         res.status(500).send("faild to save")
